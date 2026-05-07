22d09112f1eb04b36cb3d8a30ca063a5ac6ba3cb
Engineering-m <monokemsuryaengineering@gmail.com>
2026-05-08 05:16:09 +0700

    Delete js directory

--- js/app-init.js
+++
@@ -1,8 +0,0 @@
-// js/app-init.js — dimuat SEBELUM view files
-var App = {
-  token       : null,
-  user        : null,
-  currentRoute: 'dashboard',
-  state       : {},
-  views       : {}
-};
--- js/config.js
+++
@@ -1,8 +0,0 @@
-// js/config.js
-const CONFIG = {
-  API_URL      : "https://script.google.com/macros/s/AKfycbwJU_bVRkxlAiPkDjHzv5K-Q9Xy3brfhzigTEHmDpYGqHqVTEIyq_Nvhyltizu_aqrw/exec",
-  APP_NAME     : "ENGINEERING SYSTEM",
-  APP_VERSION  : "1.0.0",
-  SESSION_KEY  : "poh_token",
-  TIMEZONE     : "Asia/Jakarta"
-};
